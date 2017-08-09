636cf9127040a459536f85592831db74a815d0ba
elise654321 <elisey654321@mail.ru>
2017-08-09 13:16:27 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
-/*
-	свойствова
-	события
-	методы
-*/
-
 var btn_prev = document.querySelector('#gallery .buttons .prev');
 var btn_next = document.querySelector('#gallery .buttons .next');
 
@@ -13,23 +7,23 @@ var i = 0;
 console.log(images);
 
 btn_prev.onclick = function () {
-	images[i].className = '';
+	images[i].style.display = 'none';
 	i--;
 
 		if(i < 0){
 		i = images.length - 1;
 	}
 
-	images[i].className = 'showed';
+	images[i].style.display = 'block';
 }
 
 btn_next.onclick = function () {
-	images[i].className = 'showed';
+	images[i].style.display = 'none';
 	i++;
 
-	if(i >= images.length){
+	if(i >= images.length) {
 		i = 0;
 	}
 
-	images[i].className = '';
+	images[i].style.display = 'block';
 }
